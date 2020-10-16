d35b2626ddb5def3cd028decb010a6c5f740558a
Diaconescu Maria Raluca <44669618+Sofia190@users.noreply.github.com>
2020-10-16 17:34:48 +0300

    Added TimeApp updates

---
+++ Days_1st/admin.py
@@ -0,0 +1,41 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Day, Name, NameModel
+
+
+admin.site.register(Day)
+
+admin.site.register(Name)
+
+admin.site.register(NameModel)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Days_1st/forms.py
@@ -0,0 +1,215 @@
+
+
+
+from django.forms import ModelForm
+
+from .models import Day
+
+
+
+class DaysTillaDateForm(ModelForm):
+
+
+	class Meta:
+
+		model = Day
+
+		fields = ['first_date', 'second_date']
+
+
+
+class YearsWithAttributesEqualToaValueForm(ModelForm):
+
+
+	class Meta:
+
+		model = Day
+
+		fields = ['date_for_years_with_attributes_equal_to_a_value', 'value_for_attributes']
+
+
+
+
+class BisectYearsinaDecadeForm(ModelForm):
+
+
+	class Meta:
+
+		model = Day
+
+		fields = ['first_year_of_decade', 'second_year_of_decade']
+
+
+
+
+
+class BisectYearsinaCenturyForm(ModelForm):
+
+
+	class Meta:
+
+		model = Day
+
+		fields = ['first_year_of_century', 'second_year_of_century']
+
+
+
+
+class AttributesofnthDayForm(ModelForm):
+
+
+	class Meta:
+
+		model = Day
+
+		fields = ['date_to_sum_attributes', "nth_day_value", "nth_month_value"]
+
+
+
+
+class BisectYearsinCenturiesForm(ModelForm):
+
+
+	class Meta:
+
+		model = Day
+
+		fields = ['first_date_bisect_years_in_centuries', "second_date_bisect_years_in_centuries"]
+
+
+
+
+class MonthsandDaystillthenextSpringEquinoxForm(ModelForm):
+
+
+	class Meta:
+
+		model = Day
+
+		fields = ['first_date_next_spring_equinox', "second_date_next_spring_equinox"]
+
+
+
+
+class MonthsandDaystillthenextAutumnEquinoxForm(ModelForm):
+
+
+	class Meta:
+
+		model = Day
+
+		fields = ['first_date_next_autumn_equinox', "second_date_next_autumn_equinox"]
+
+
+
+
+
+
+class PeriodsbetweenDateswithTotalLunarEclipseForm(ModelForm):
+
+
+	class Meta:
+
+		model = Day
+
+		fields = ['first_date_with_total_lunar_eclipse', "second_date_with_total_lunar_eclipse"]
+
+
+
+
+
+class Find_the_equivalent_of_timezone_with_USA_Form(ModelForm):
+
+
+	class Meta:
+
+		model = Day
+
+		fields = ['inTimeDelta_hours', "inTZObject_name", "datetime_instance_var",]
+
+
+
+
+
+
+class Find_the_difference_of_timezone_with_USA_Form(ModelForm):
+
+
+	class Meta:
+
+		model = Day
+
+		fields = ['inTimeDelta_hours_diff', "inTZObject_name_diff", "datetime_instance_var_diff",]
+
+
+
+
+class Find_equivalent_with_timezone_Form(ModelForm):
+
+	class Meta:
+
+		model = Day
+
+		fields = ['inTimeDelta_hours', "inTZObject_name_field", "datetime_instance_var",]
+
+
+	# def clean_inTimeDelta_hours(self):
+
+	# 	inTimeDelta_hours = self.cleaned_data.get('inTimeDelta_hours')
+
+	# 	return inTimeDelta_hours
+
+
+
+
+
+
+class Find_difference_of_timezone_from_UTC_Form(ModelForm):
+
+	class Meta:
+
+		model = Day
+
+		fields = ['your_address', "inTZObject_name_field", "datetime_instance_var",]
+
+
+
+
+	
+class find_difference_between_two_timezones_Form(ModelForm):
+
+	class Meta:
+
+		model = Day
+
+		fields = ['your_address', "address_to_search", ]
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
